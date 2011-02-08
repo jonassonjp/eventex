ef4e5d50ef940e240a5b8e687556e1ed78c842df
Jonas Pereira <jonassonjp@gmail.com>
2011-02-08 11:23:12 -0300

    Criado uma aplicacao CORE; Criado o método homepage na view da app core

---
+++ core/views.py
@@ -0,0 +1,4 @@
+from django.http import HttpResponse
+
+def homepage(request):
+	return HttpResponse("Bem vindo ao Eventex")	
--- urls.py
+++ urls.py
@@ -1,10 +1,12 @@
 from django.conf.urls.defaults import *
+from core.views import *
 
 # Uncomment the next two lines to enable the admin:
 # from django.contrib import admin
 # admin.autodiscover()
 
 urlpatterns = patterns('',
+    (r'^$', homepage),
     # Example:
     # (r'^eventex/', include('eventex.foo.urls')),
 
