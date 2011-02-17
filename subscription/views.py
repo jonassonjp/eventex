878ae4441ea2caffb3fe13652ef6d4a3b7e14726
Jonas Pereira <jonassonjp@gmail.com>
2011-02-16 21:12:32 -0300

    Criou o arquivo base.html; Alterou index.html para herdar de base.html

--- subscription/views.py
+++ subscription/views.py
@@ -1,1 +1,6 @@
-# Create your views here.
+from django.shortcuts import render_to_response
+from django.template import RequestContext
+
+def subscribe(request):
+	context = RequestContext(request)
+	return render_to_response('new.html', context)
