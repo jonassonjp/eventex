05cbbdc1679d39256925db821687a1fa2950315d
Jonas Pereira <jonassonjp@gmail.com>
2011-02-18 22:44:11 -0300

    Adicionado a app ADMIN ao projeto; Criado o arquivo core/fixtures/initial_data.json para inicializar com os usuários corretos.

--- urls.py
+++ urls.py
@@ -3,8 +3,8 @@ from django.conf import settings
 from core.views import *
 
 # Uncomment the next two lines to enable the admin:
-# from django.contrib import admin
-# admin.autodiscover()
+from django.contrib import admin
+admin.autodiscover()
 
 urlpatterns = patterns('',
     (r'^$', homepage),
@@ -16,7 +16,7 @@ urlpatterns = patterns('',
     # (r'^admin/doc/', include('django.contrib.admindocs.urls')),
 
     # Uncomment the next line to enable the admin:
-    # (r'^admin/', include(admin.site.urls)),
+    (r'^admin/', include(admin.site.urls)),
 )
 
 if settings.DEBUG:
